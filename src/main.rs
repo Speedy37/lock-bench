52bcca2cec6a84c3a38958dcf1e70c4d128fec96
Vincent Rouillé <vincent@speedy37.fr>
2020-01-10 20:19:14 +0100

    criterion + cpu times

--- src/main.rs
+++ src/main.rs
@@ -1,53 +1,75 @@
 use std::{iter, sync::Barrier, time};
 
+use criterion::*;
 use crossbeam_utils::{thread::scope, CachePadded};
 
-fn main() {
-    let mut args = std::env::args()
-        .skip(1)
-        .map(|it| it.parse::<u32>().unwrap());
-
-    let options = Options {
-        n_threads: args.next().unwrap(),
-        n_locks: args.next().unwrap(),
-        n_ops: args.next().unwrap(),
-        n_rounds: args.next().unwrap(),
-    };
-    println!("{:#?}\n", options);
-
-    bench::<mutexes::Std>(&options);
-    bench::<mutexes::ParkingLot>(&options);
-    bench::<mutexes::Spin>(&options);
-    bench::<mutexes::AmdSpin>(&options);
-
-    println!();
-    bench::<mutexes::Std>(&options);
-    bench::<mutexes::ParkingLot>(&options);
-    bench::<mutexes::Spin>(&options);
-    bench::<mutexes::AmdSpin>(&options);
+fn mutex(c: &mut Criterion) {
+    mutex_timings(c, "duration", TimeKind::Real);
+    mutex_timings(c, "cpu", TimeKind::KernelPlusUser);
+    mutex_timings(c, "kernel", TimeKind::Kernel);
+    mutex_timings(c, "user", TimeKind::User);
 }
 
-fn bench<M: Mutex>(options: &Options) {
-    let mut times = (0..options.n_rounds)
-        .map(|_| run_bench::<M>(options))
-        .collect::<Vec<_>>();
-    times.sort();
-
-    let avg = times.iter().sum::<time::Duration>() / options.n_rounds;
-    let min = times[0];
-    let max = *times.last().unwrap();
-
-    let avg = format!("{:?}", avg);
-    let min = format!("{:?}", min);
-    let max = format!("{:?}", max);
-
-    println!(
-        "{:<20} avg {:<12} min {:<12} max {:<12}",
-        M::LABEL,
-        avg,
-        min,
-        max
-    )
+fn mutex_timings(c: &mut Criterion, name: &str, kind: TimeKind) {
+    let mut group = c.benchmark_group(name);
+    for input in vec![
+        (
+            Options {
+                n_threads: 32,
+                n_locks: 2,
+                n_ops: 10000,
+            },
+            "extreme contention",
+        ),
+        (
+            Options {
+                n_threads: 32,
+                n_locks: 64,
+                n_ops: 10000,
+            },
+            "heavy contention",
+        ),
+        (
+            Options {
+                n_threads: 32,
+                n_locks: 1000,
+                n_ops: 10000,
+            },
+            "light contention",
+        ),
+        (
+            Options {
+                n_threads: 32,
+                n_locks: 1000000,
+                n_ops: 10000,
+            },
+            "no contention",
+        ),
+    ]
+    .iter()
+    {
+        let (input, p) = input;
+        group.bench_with_input(
+            BenchmarkId::new(*p, mutexes::Std::LABEL),
+            &input,
+            |b, options| b.iter_custom(|n| run_bench::<mutexes::Std>(options, n, kind)),
+        );
+        group.bench_with_input(
+            BenchmarkId::new(*p, mutexes::ParkingLot::LABEL),
+            &input,
+            |b, options| b.iter_custom(|n| run_bench::<mutexes::ParkingLot>(options, n, kind)),
+        );
+        group.bench_with_input(
+            BenchmarkId::new(*p, mutexes::Spin::LABEL),
+            &input,
+            |b, options| b.iter_custom(|n| run_bench::<mutexes::Spin>(options, n, kind)),
+        );
+        group.bench_with_input(
+            BenchmarkId::new(*p, mutexes::AmdSpin::LABEL),
+            &input,
+            |b, options| b.iter_custom(|n| run_bench::<mutexes::AmdSpin>(options, n, kind)),
+        );
+    }
 }
 
 #[derive(Debug)]
@@ -55,7 +77,6 @@ struct Options {
     n_threads: u32,
     n_locks: u32,
     n_ops: u32,
-    n_rounds: u32,
 }
 
 fn random_numbers(seed: u32) -> impl Iterator<Item = u32> {
@@ -73,7 +94,41 @@ trait Mutex: Sync + Send + Default {
     fn with_lock(&self, f: impl FnOnce(&mut u32));
 }
 
-fn run_bench<M: Mutex>(options: &Options) -> time::Duration {
+#[derive(Debug, Clone, Copy)]
+enum TimeKind {
+    Kernel,
+    User,
+    KernelPlusUser,
+    Real,
+}
+
+struct Times {
+    kernel_time: time::Duration,
+    user_time: time::Duration,
+    elapsed: time::Duration,
+}
+impl Times {
+    fn elapsed(t0: time::Instant, r0: RUsage) -> Times {
+        let elapsed = t0.elapsed();
+        let r1 = rusage::getrusage();
+        Times {
+            kernel_time: r1.kernel_time - r0.kernel_time,
+            user_time: r1.user_time - r0.user_time,
+            elapsed,
+        }
+    }
+
+    fn get(&self, kind: TimeKind) -> time::Duration {
+        match kind {
+            TimeKind::Kernel => self.kernel_time,
+            TimeKind::User => self.user_time,
+            TimeKind::KernelPlusUser => self.kernel_time + self.user_time,
+            TimeKind::Real => self.elapsed,
+        }
+    }
+}
+
+fn run_bench<M: Mutex>(options: &Options, n: u64, kind: TimeKind) -> time::Duration {
     let locks = &(0..options.n_locks)
         .map(|_| CachePadded::new(M::default()))
         .collect::<Vec<_>>();
@@ -92,7 +147,7 @@ fn run_bench<M: Mutex>(options: &Options) -> time::Duration {
                 let indexes = random_numbers(thread_seed)
                     .map(|it| it % options.n_locks)
                     .map(|it| it as usize)
-                    .take(options.n_ops as usize);
+                    .take(n as usize);
                 for idx in indexes {
                     locks[idx].with_lock(|cnt| *cnt += 1);
                 }
@@ -102,20 +157,116 @@ fn run_bench<M: Mutex>(options: &Options) -> time::Duration {
 
         std::thread::sleep(time::Duration::from_millis(100));
         start_barrier.wait();
-        let start = time::Instant::now();
+        let r0 = rusage::getrusage();
+        let t0 = time::Instant::now();
         end_barrier.wait();
-        let elapsed = start.elapsed();
+        let elapsed = Times::elapsed(t0, r0);
 
         let mut total = 0;
         for lock in locks.iter() {
             lock.with_lock(|cnt| total += *cnt);
         }
-        assert_eq!(total, options.n_threads * options.n_ops);
+        assert_eq!(total, options.n_threads * (n as u32));
 
         elapsed
     })
     .unwrap();
-    elapsed
+    elapsed.get(kind)
+}
+
+struct RUsage {
+    kernel_time: time::Duration,
+    user_time: time::Duration,
+}
+#[cfg(unix)]
+mod rusage {
+    use super::RUsage;
+    use std::time::Duration;
+
+    fn to_duration(tv: libc::timeval) -> Duration {
+        // resolution: 1000ns
+        let usec = ((tv.tv_sec as u64) * 1_000_000) + (tv.tv_usec as u64);
+        return Duration::new(usec / 1_000_000, ((usec % 1_000_000) as u32) * 1_000);
+    }
+
+    pub(crate) fn getrusage() -> RUsage {
+        let mut rusage = libc::rusage {
+            ru_utime: libc::timeval {
+                tv_sec: 0,
+                tv_usec: 0,
+            },
+            ru_stime: libc::timeval {
+                tv_sec: 0,
+                tv_usec: 0,
+            },
+            ru_maxrss: 0,
+            ru_ixrss: 0,
+            ru_idrss: 0,
+            ru_isrss: 0,
+            ru_minflt: 0,
+            ru_majflt: 0,
+            ru_nswap: 0,
+            ru_inblock: 0,
+            ru_oublock: 0,
+            ru_msgsnd: 0,
+            ru_msgrcv: 0,
+            ru_nsignals: 0,
+            ru_nvcsw: 0,
+            ru_nivcsw: 0,
+        };
+        let ok = unsafe { libc::getrusage(libc::RUSAGE_SELF, &mut rusage) };
+        if ok != 0 {
+            panic!("{:?}", std::io::Error::last_os_error());
+        }
+        RUsage {
+            kernel_time: to_duration(rusage.ru_stime),
+            user_time: to_duration(rusage.ru_utime),
+        }
+    }
+}
+#[cfg(windows)]
+mod rusage {
+    use super::RUsage;
+    use std::time::Duration;
+
+    use winapi::shared::minwindef::FILETIME;
+    use winapi::um::processthreadsapi::GetCurrentProcess;
+    use winapi::um::processthreadsapi::GetProcessTimes;
+
+    fn zero() -> FILETIME {
+        FILETIME {
+            dwLowDateTime: 0,
+            dwHighDateTime: 0,
+        }
+    }
+
+    fn to_duration(ft: FILETIME) -> Duration {
+        // resolution: 100ns
+        let ft100 = ((ft.dwHighDateTime as u64) << 32) + ft.dwLowDateTime as u64;
+        return Duration::new(ft100 / 10_000_000, ((ft100 * 100) % 1000_000_000) as u32);
+    }
+
+    pub(crate) fn getrusage() -> RUsage {
+        let mut kernel_time = zero();
+        let mut user_time = zero();
+        let process = unsafe { GetCurrentProcess() };
+        let ok = unsafe {
+            GetProcessTimes(
+                process,
+                &mut zero(),
+                &mut zero(),
+                &mut kernel_time,
+                &mut user_time,
+            )
+        };
+        if ok == 0 {
+            panic!("{:?}", std::io::Error::last_os_error());
+        }
+        RUsage {
+            kernel_time: to_duration(kernel_time),
+            user_time: to_duration(kernel_time),
+        }
+    }
 }
 
 mod mutexes {
@@ -221,3 +372,6 @@ mod amd_spinlock {
         }
     }
 }
+
+criterion_group!(benches, mutex);
+criterion_main!(benches);
